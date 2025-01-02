778030e6d94cc55dfa4f604d4850a73016ac77e1
niteshyadav7 <nitesh0709yadav@gmail.com>
2025-01-02 13:19:26 +0530

    basic auth setup

--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -1,6 +1,11 @@
-import React, { useRef, useState } from "react";
+import React, { use, useRef, useState } from "react";
 import Header from "./Header";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { checkValidData } from "../utils/validate";
+import { auth } from "../utils/firebase";
 
 const Login = () => {
   const [isLoggedIn, setLoggedIn] = useState(true);
@@ -12,6 +17,43 @@ const Login = () => {
   const handleSubmitButton = () => {
     const message = checkValidData(email.current.value, password.current.value);
     setErrorMessage(message);
+    if (message) return;
+    if (!isLoggedIn) {
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode + " " + errorMessage);
+        });
+    } else {
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+
+          console.log(errorCode + " " + errorMessage);
+        });
+    }
   };
 
   const handleClick = () => {
--- netflix-gpt/src/utils/firebase.js
+++ netflix-gpt/src/utils/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +14,11 @@ const firebaseConfig = {
   storageBucket: "netflixgpt-8a0d2.firebasestorage.app",
   messagingSenderId: "295166390118",
   appId: "1:295166390118:web:12b444d9deae71e37c62a7",
-  measurementId: "G-Q3E6Y1FQEW"
+  measurementId: "G-Q3E6Y1FQEW",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+// const auth = getAuth();
+export const auth = getAuth();
